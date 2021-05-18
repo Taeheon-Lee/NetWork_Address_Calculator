e37ccd1e0467d10bf230952272b617dc9b772462
TaeHeon-Lee <taeheon714@gmail.com>
2021-05-18 12:47:07 +0900

    tmp2

--- casting.py
+++ casting.py
@@ -7,14 +7,13 @@ def de_to_bi(input_num):
     if input_num in (0, 1):
         return input_num
     if input_num < 0:
-        print("The number should be positive")
-        return None
+        raise ValueError("The number should be positive")
     answer = ""
     while input_num != 0:
         tmp = input_num % 2
         input_num = input_num // 2
         answer = str(tmp) + answer
-    return int(answer)
+    return answer
 
 def bi_to_de(input_num):
     """
@@ -23,12 +22,11 @@ def bi_to_de(input_num):
     if input_num in (0, 1):
         return input_num
     if input_num < 0:
-        print("The number should be positive")
-        return None
+        raise ValueError("The number should be positive")
     answer = 0
     fac = 0.5
     while input_num != 0:
         fac = fac * 2
         answer = answer + input_num % 10 * fac
         input_num = input_num // 10
-    return int(answer)
+    return answer
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+"main function"
+
 import casting
 
 s1 = input("Please type network address: ")
 s2 = input("Please type subnet mask: ")
 
-lst1 = map(int, s1.split("."))
-lst2 = map(int, s2.split("."))
+net_add = s1.split(".")
+sub_mas = s2.split(".")
+
+check_arr = ["128", "192", "224", "240", "248", "252", "255"]
+for elem in net_add:
+    if elem < 0 or elem > 255:
+        raise ValueError("The network adress is not correct")
+for elem in sub_mas:
+    if elem not in check_arr:
+        raise ValueError("The subnetmask is not correct")
+answer = []
+i = 0
+while sub_mas[i] == "255":
+    answer.append(net_add[i])
+    i += 1
+bi_sub = casting.de_to_bi(int(sub_mas[i]))
+sep = bi_sub.index("0")
+bi_net = casting.de_to_bi(int(net_add[i]))
